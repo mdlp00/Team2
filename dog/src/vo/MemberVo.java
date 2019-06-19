0969a6f3ac1b5d45207459349fdb011fe1e4a08c
Koala <Koala@DESKTOP-C1BUE0M>
2019-06-19 10:08:50 +0900

    2019/06/19 안다예 MemberVo 추가

--- dog/src/vo/MemberVo.java
+++ dog/src/vo/MemberVo.java
@@ -1,5 +1,52 @@
 package vo;
 
+import java.sql.*;
 public class MemberVo {
-
+	
+	private String sid;
+	private String spw;
+	private String sname;
+	private String sbirth;
+	private String sadd;
+	private String sphone;
+	
+	
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public String getSpw() {
+		return spw;
+	}
+	public void setSpw(String spw) {
+		this.spw = spw;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSbirth() {
+		return sbirth;
+	}
+	public void setSbirth(String sbirth) {
+		this.sbirth = sbirth;
+	}
+	public String getSadd() {
+		return sadd;
+	}
+	public void setSadd(String sadd) {
+		this.sadd = sadd;
+	}
+	public String getSphone() {
+		return sphone;
+	}
+	public void setSphone(String sphone) {
+		this.sphone = sphone;
+	}
+	
+	
 }
