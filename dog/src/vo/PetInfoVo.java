b36e0076c7ae0e9a8183153c1bbb7e6fedad296f
mdlp0 <mdlp0@DESKTOP-P76QBBO>
2019-06-20 10:37:22 +0900

    2019-06-20 양희준 PetInfoVo.java 추가

---
+++ dog/src/vo/PetInfoVo.java
@@ -0,0 +1,35 @@
+package vo;
+
+public class PetInfoVo {
+	private String m_id;
+	private int ps_no;
+	private int pa_no;
+	private int pk_no;
+	
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public int getPs_no() {
+		return ps_no;
+	}
+	public void setPs_no(int ps_no) {
+		this.ps_no = ps_no;
+	}
+	public int getPa_no() {
+		return pa_no;
+	}
+	public void setPa_no(int pa_no) {
+		this.pa_no = pa_no;
+	}
+	public int getPk_no() {
+		return pk_no;
+	}
+	public void setPk_no(int pk_no) {
+		this.pk_no = pk_no;
+	}
+	
+	
+}
